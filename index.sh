e649bafccbfec256fc78bf03662f5528e90dadb0
Alain Armand <aarmand.inbox@gmail.com>
2020-11-22 16:18:04 +0100

    broken

--- index.sh
+++ index.sh
@@ -1,32 +1,237 @@
 #!/bin/sh
 
-DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+PWD=$(pwd)
 
-if [ $# -ge 1 ]
-then
+
+deleteML() {
+    find . -type f -name '*.mli' -print -delete
+    find . -type f -name '*.ml' -print -delete
+};
+deleteRE() {
+    find . -type f -name '*.rei' -print -delete
+    find . -type f -name '*.re' -print -delete
+};
+
+
+ml2re() {
+    ext=("ml")
+    from=(".ml");
+    to=(".re");
+    shopt -s nullglob
+    # Check for file
+    for ext in $ext; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "Found these $from files:"
+            printf "%s\n" "${files[@]}"
+            find . -name $from| while read filename; do bsrefmt --parse $from --print $to $filename >${filename%.$from}.$to; done
+            
+        else
+            echo 👎"${RED}Oops, no $from files found 👎\n"
+            break
+        fi
+        
+    done
+    
+    # Print out result
+    for ext in ml; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "New $to files:"
+            printf "%s\n" "${files[@]}"
+            
+        else
+            echo 👎"${RED}Oops, no $from files found 👎\n"
+            break
+        fi
+        
+    done    
+}
+
+re2ml() {
+    ext=("re")
+    ml=("ml")
+    from=(".re");
+    to=(".ml");
+    shopt -s nullglob
+    # Check for file
+    for ext in $ext; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "Found these $from files:"
+            printf "%s\n" "${files[@]}"
+            find . -name $from| while read filename; do bsrefmt --parse $from --print $to $filename >${filename%.$from}.$to; done
+            
+        else
+            echo "Oops, no $from files found"
+            break
+        fi
+        
+    done
+    
+    # Print out result
+    ext=("ml")
+    for ext in ml; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "New $ml files:"
+            printf "%s\n" "${files[@]}"
+            
+        else
+            echo "Oops, no $from files found"
+            break
+        fi
+        
+    done    
+}
+
+mli2rei() {
+    ext=("mli")
+    from=(".mli");
+    to=(".rei");
+    shopt -s nullglob
+    # Check for file
+    for ext in $ext; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "Found these $from files:"
+            printf "%s\n" "${files[@]}"
+            find . -name $from| while read filename; do bsrefmt --parse $from --print $to $filename >${filename%.$from}.$to; done
+            
+        else
+            echo "Oops, no $from files found"
+            break
+        fi
+        
+    done
+    
+    # Print out result
+    for ext in ml; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "New $to files:"
+            printf "%s\n" "${files[@]}"
+            
+        else
+            echo "Oops, no $from files found"
+            break
+        fi
+        
+    done
+}
+
+rei2mli() {
+    ext=("rei")
+    from=(".rei");
+    to=(".mli");
+    shopt -s nullglob
+    # Check for file
+    for ext in $ext; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "Found these $from files:"
+            printf "%s\n" "${files[@]}"
+            find . -name $from| while read filename; do bsrefmt --parse $from --print $to $filename >${filename%.$from}.$to; done
+            
+        else
+            echo "Oops, no $from files found"
+            break
+        fi
+        
+    done
+    
+    # Print out result
+    for ext in ml; do
+        files=( *."$ext" )
+        printf 'number of %s files: %d\n' "$ext" "${#files[@]}"
+        if (( ${#files[@]} )); then
+            echo "New $to files:"
+            printf "%s\n" "${files[@]}"
+            
+        else
+            echo "Oops, no $from files found"
+            break
+        fi
+        
+    done
+    
+}
+
+options() {
+    
+    PS3="Select the operation: "
+    
+    select opt in ml2re re2ml mli2rei rei2mli deleteML deleteRE quit; do
+        
+        case $opt in
+            ml2re) ml2re
+            ;;
+            mli2rei) mli2rei
+            ;;
+            re2ml) re2ml
+            ;;
+            rei2mli) rei2mli
+            ;;
+            deleteML) deleteML
+            ;;
+            deleteRE) deleteRE
+            ;;
+            quit)
+                break
+            ;;
+            *)
+                echo "Invalid option $REPLY. Enter 6 to quit"
+            ;;
+        esac
+    done
+}
+
+
+
+if [ $# -ge 1 ]; then
     while [ -n "$1" ]; do # while loop starts
         
         case "$1" in
             
-            ml2re) . "$DIR/scripts/ml2re.sh";;
+            ml2re)
+                echo "Converts .ml file to .re syntax"
+            ml2re;;
             
-            mli2rei) . "$DIR/scripts/mli2rei.sh";;
+            mli2rei)
+                echo "Converts .mli file to .rei syntax"
+            mli2rei;;
             
-            re2ml) . "$DIR/scripts/re2ml.sh";;
+            re2ml)
+                echo "Converts .re file to .ml syntax"
+            re2ml;;
             
-            rei2mli) . "$DIR/scripts/rei2mli.sh";;
+            rei2mli)
+                echo "Converts .rei file to .mli syntax"
+            rei2mli;;
             
-            delete) . "$DIR/scripts/delete.sh";;
+            deleteML)
+                echo "Deletes .ml/.mli files. Only use after you are finished converting to .re/.rei"
+            deleteML;;
             
-            *) echo "Invalid option. Options are mli2rei, rei2mli, ml2re, re2ml, delete or quit to exit"
-            . "$DIR/scripts/options.sh";;
+            deleteRE)
+                echo "Deletes .re/.rei files. Only use after you are finished converting to .ml/.mli"
+                
+            deleteRE;;
+            
+            *) echo "Invalid option. Options are mli2rei, rei2mli, ml2re, re2ml, deleteML,deleteRE or quit to exit"
+            options;;
             
         esac
         
         shift
-        
     done
 else
-    . "$DIR/scripts/options.sh"
+    options
 fi
--- scripts/delete.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# set ml $(fd -e)
-# echo $ml
-
-fd -e mli -x rm -rf
-fd -e ml -x rm -rf
